0c8806d7a23cc39c44eead470c2478b4231d7ae5
Huy Le <huyle2@illinois.edu>
2015-11-13 01:10:54 -0600

    first commit

---
+++ app/src/main/java/edu/illinois/sba/camera2raw/Native.java
@@ -0,0 +1,10 @@
+package edu.illinois.sba.camera2raw;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by meowle on 11/6/2015.
+ */
+public class Native {
+    public native static void saveImageAsCSV(String str, ByteBuffer byteBuffer, int width, int height);
+}
---
+++ app/src/main/jni/edu_illinois_sba_camera2raw_Native.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include "edu_illinois_sba_camera2raw_Native.h"
+
+JNIEXPORT void JNICALL Java_edu_illinois_sba_camera2raw_Native_saveImageAsCSV
+  (JNIEnv *env, jclass, jstring jstr, jobject jobj, jint width , jint height) {
+	short *buf = (short *) env->GetDirectBufferAddress(jobj);
+	const char *path = env->GetStringUTFChars(jstr, nullptr);
+	std::ofstream output(path);
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			output << *(buf++) << ",";
+		}
+		output << "\n";
+	}
+	output.close();
+	env->ReleaseStringUTFChars(jstr, path);
+  }
+
